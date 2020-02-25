50124b9534828b00653e7bef435a5515c01d7c91
Mahantayya Hiremath <mahantesh21@gmail.com>
2020-02-25 22:14:40 +0000

    Update

--- springbootrestdemo/src/main/java/com/springboot/rest/example/springbootrestdemo/SpringbootrestdemoApplication.java
+++ springbootrestdemo/src/main/java/com/springboot/rest/example/springbootrestdemo/SpringbootrestdemoApplication.java
@@ -2,9 +2,7 @@ package com.springboot.rest.example.springbootrestdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan("com.springboot.rest.example.domain")
 @SpringBootApplication
 public class SpringbootrestdemoApplication {
 
