10c2438ce264018ab9e2be9144a6b81562580eda
Mahantayya Hiremath <mahantesh21@gmail.com>
2020-02-26 01:34:06 +0000

    cosmetic

--- springbootrestdemo/src/main/java/com/springboot/rest/example/springbootrestdemo/RestClient.java
+++ springbootrestdemo/src/main/java/com/springboot/rest/example/springbootrestdemo/RestClient.java
@@ -1,8 +1,5 @@
 package com.springboot.rest.example.springbootrestdemo;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
